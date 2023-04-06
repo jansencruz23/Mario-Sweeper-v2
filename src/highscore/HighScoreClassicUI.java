2013fec4c09ed7fd5aadd82ed816cc50abcf2be5
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-04-06 21:28:06 +0800

    Fix display leaderboards

--- src/highscore/HighScoreClassicUI.java
+++ src/highscore/HighScoreClassicUI.java
@@ -1,27 +1,33 @@
 package highscore;
 
 import components.CirclePanel;
-import contracts.IHighScore;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.geom.RoundRectangle2D;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
 
 public class HighScoreClassicUI  extends javax.swing.JFrame {
-    private ArrayList<HighScoreClassic> leaderboards;
-    Leaderboards l;
+    final int MAX_LEADERBOARD_SIZE = 3;
+    final int EMPTY = 0;
+    private ArrayList<HighScoreClassic> leaderboardsList;
+    Leaderboards leaderboards;
     
     public HighScoreClassicUI(Leaderboards leaderboards) {
-        this.l = leaderboards;
+        this.leaderboards = leaderboards;
+        this.leaderboardsList = leaderboards.getLeaderboards();
         initComponents();
         setLocationRelativeTo(null);
         displayHighScore();
-        //displayLeaderboard();
+        displayColumn("score");
+        displayColumn("name");
+        displayColumn("lives");
+        displayColumn("time");
     }
 
     @SuppressWarnings("unchecked")
@@ -48,22 +54,23 @@ public class HighScoreClassicUI  extends javax.swing.JFrame {
         lblLives1 = new javax.swing.JLabel();
         lblTime1 = new javax.swing.JLabel();
         jLabel20 = new javax.swing.JLabel();
-        jLabel18 = new javax.swing.JLabel();
-        jLabel15 = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
+        lblScore2 = new javax.swing.JLabel();
+        lblName2 = new javax.swing.JLabel();
+        lblLives2 = new javax.swing.JLabel();
+        lblTime2 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        lblScore3 = new javax.swing.JLabel();
+        lblName3 = new javax.swing.JLabel();
+        lblLives3 = new javax.swing.JLabel();
+        lblTime3 = new javax.swing.JLabel();
         lblTitle = new javax.swing.JLabel();
         btnExit = new javax.swing.JButton();
+        lblBg = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(500, 400));
+        setMinimumSize(new java.awt.Dimension(500, 440));
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(500, 400));
+        setPreferredSize(new java.awt.Dimension(500, 440));
         setResizable(false);
         setSize(new java.awt.Dimension(500, 500));
         getContentPane().setLayout(null);
@@ -90,24 +97,48 @@ public class HighScoreClassicUI  extends javax.swing.JFrame {
 
         jLabel8.setText("1.");
         panelHighScore.add(jLabel8);
+
+        lblScore1.setName("score"); // NOI18N
         panelHighScore.add(lblScore1);
+
+        lblName1.setName("name"); // NOI18N
         panelHighScore.add(lblName1);
+
+        lblLives1.setName("lives"); // NOI18N
         panelHighScore.add(lblLives1);
+
+        lblTime1.setName("time"); // NOI18N
         panelHighScore.add(lblTime1);
 
         jLabel20.setText("2.");
         panelHighScore.add(jLabel20);
-        panelHighScore.add(jLabel18);
-        panelHighScore.add(jLabel15);
-        panelHighScore.add(jLabel14);
-        panelHighScore.add(jLabel11);
+
+        lblScore2.setName("score"); // NOI18N
+        panelHighScore.add(lblScore2);
+
+        lblName2.setName("name"); // NOI18N
+        panelHighScore.add(lblName2);
+
+        lblLives2.setName("lives"); // NOI18N
+        panelHighScore.add(lblLives2);
+
+        lblTime2.setName("time"); // NOI18N
+        panelHighScore.add(lblTime2);
 
         jLabel10.setText("3.");
         panelHighScore.add(jLabel10);
-        panelHighScore.add(jLabel7);
-        panelHighScore.add(jLabel4);
-        panelHighScore.add(jLabel6);
-        panelHighScore.add(jLabel3);
+
+        lblScore3.setName("score"); // NOI18N
+        panelHighScore.add(lblScore3);
+
+        lblName3.setName("name"); // NOI18N
+        panelHighScore.add(lblName3);
+
+        lblLives3.setName("lives"); // NOI18N
+        panelHighScore.add(lblLives3);
+
+        lblTime3.setName("time"); // NOI18N
+        panelHighScore.add(lblTime3);
 
         getContentPane().add(panelHighScore);
         panelHighScore.setBounds(40, 130, 410, 260);
@@ -115,7 +146,7 @@ public class HighScoreClassicUI  extends javax.swing.JFrame {
         lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblTitle.setText("SUPER PLAYERS");
         getContentPane().add(lblTitle);
-        lblTitle.setBounds(50, 70, 378, 48);
+        lblTitle.setBounds(50, 70, 390, 48);
 
         btnExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/exit.png"))); // NOI18N
         btnExit.setBorderPainted(false);
@@ -128,6 +159,8 @@ public class HighScoreClassicUI  extends javax.swing.JFrame {
         });
         getContentPane().add(btnExit);
         btnExit.setBounds(400, 20, 60, 50);
+        getContentPane().add(lblBg);
+        lblBg.setBounds(-50, -380, 670, 1110);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -139,7 +172,7 @@ public class HighScoreClassicUI  extends javax.swing.JFrame {
     
     private void displayHighScore(){
         try {
-            for(HighScoreClassic h : l.getLeaderboards()) {
+            for(HighScoreClassic h : leaderboards.getLeaderboards()) {
                 System.out.println(h.getName());
         }
         } catch (Exception ex) {
@@ -147,32 +180,65 @@ public class HighScoreClassicUI  extends javax.swing.JFrame {
         }
     }
     
-    private void displayLeaderboard() {
-        lblScore1.setText(leaderboards.get(0).toString());
+    private void displayColumn(String name) {
+        if(leaderboardsList.size() == EMPTY) 
+            return;
+        
+        else if(leaderboardsList.size() >= MAX_LEADERBOARD_SIZE){
+            int index = 0;            
+                while(index < MAX_LEADERBOARD_SIZE) {
+                    for(Component component : panelHighScore.getComponents()){
+                    var lbl = (JLabel) component;
+                    if(lbl.getName() != null)
+                        if(lbl.getName().equals(name)){
+                            switch(name){
+                                case "score": 
+                                    lbl.setText(leaderboardsList.get(index).getScore() + "");
+                                    lbl.setName("done");
+                                    break;
+                                case "name":
+                                    lbl.setText(leaderboardsList.get(index).getName());
+                                    lbl.setName("done");
+                                    break;
+                                case "lives":
+                                  lbl.setText(leaderboardsList.get(index).getLivesLeft() + "");
+                                  lbl.setName("done");
+                                  break;  
+                                case "time":
+                                    lbl.setText(leaderboardsList.get(index).getTime() + "");
+                                    lbl.setName("done");
+                                    break;
+                            }
+                            index++;
+                        }
+                }
+            }
+        }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnExit;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel15;
-    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel20;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel lblBg;
     private javax.swing.JLabel lblLives1;
+    private javax.swing.JLabel lblLives2;
+    private javax.swing.JLabel lblLives3;
     private javax.swing.JLabel lblName1;
+    private javax.swing.JLabel lblName2;
+    private javax.swing.JLabel lblName3;
     private javax.swing.JLabel lblScore1;
+    private javax.swing.JLabel lblScore2;
+    private javax.swing.JLabel lblScore3;
     private javax.swing.JLabel lblTime1;
+    private javax.swing.JLabel lblTime2;
+    private javax.swing.JLabel lblTime3;
     private javax.swing.JLabel lblTitle;
     private javax.swing.JPanel panelHighScore;
     // End of variables declaration//GEN-END:variables
